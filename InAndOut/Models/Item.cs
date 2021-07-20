042f7d268c530d6c65d1a93e205967b062a59a5f
dmiljan <miljan_drakul@yahoo.com>
2021-07-20 11:35:52 +0200

    Add project files.

---
+++ InAndOut/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; //[Key]
+
+namespace InAndOut.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Borrower { get; set; }
+    }
+}
